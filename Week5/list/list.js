9683bd53181744a2f80d7510e80be425f07c1447
ed-g <edward.groth@gmail.com>
2015-12-08 18:39:57 -0800

    changes

--- Week5/list/list.js
+++ Week5/list/list.js
@@ -1,8 +1,9 @@
 
 var hello = function (){
-  var text = document.getElementById(contentId);
-  alert("button clicked");
-};
+  var text = document.getElementById("new_list_text");
+  alert("button clicked", text); };
+
 ( function () {
+  // this will be an init function, setting up any call backs etc.
 }
  ());
